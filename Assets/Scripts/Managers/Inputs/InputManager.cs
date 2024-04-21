063766c8508a362ae67c5481c6bde1e2d92585ac
Althep <pnr1367@gmail.com>
2024-04-21 20:21:29 +0900

    AttackWaitFunctionChange
    시간함수로 교체

--- Assets/Scripts/Managers/Inputs/InputManager.cs
+++ Assets/Scripts/Managers/Inputs/InputManager.cs
@@ -19,13 +19,16 @@ public class InputManager : MonoBehaviour
     float moveSpeed = 4f;
 
     public MoveState playerMoveState = MoveState.idle;
+    bool CanInput;
 
     GameObject playerObj;
     GameObject[] targets;
-    
+
     PlayerState playerState;
     MapMake mapScript;
+
     KeyCode lastInput;
+    KeyCode currentKey;
     Vector3 moveDirection;
     Vector3 playerPos;
 
@@ -34,37 +37,53 @@ public class InputManager : MonoBehaviour
         playerObj = GameManager.instance.playerObj;
         mapScript = GameManager.instance.gameObject.transform.GetComponent<MapMake>();
         playerState = playerObj.transform.GetComponent<PlayerState>();
-        
+
     }
     private void FixedUpdate()
     {
-        /*
-        if (Input.anyKey)
+        if(playerMoveState == MoveState.idle&&Input.anyKeyDown)
         {
-            foreach(KeyCode keycode in System.Enum.GetValues(typeof(KeyCode)))
+            OnkeyPlayerMove();
+        }
+        
+
+
+    }
+    /*
+    public bool KeyCodeCompare()
+    {
+        // 현재 입력된 키와 마지막으로 입력된 키 비교
+        if (Input.anyKeyDown)
+        {
+            foreach (KeyCode keyCode in System.Enum.GetValues(typeof(KeyCode)))
             {
-                if (Input.GetKeyDown(keycode))
+                if (Input.GetKeyDown(keyCode))
                 {
-                    lastInput = keycode;
-                    Debug.Log(lastInput);
-                    Debug.Log(keycode);
+                    if (keyCode == lastInput && lastInputTime+0.5f<Time.time)
+                    {
+                        //Debug.Log("현재 입력된 키는 마지막으로 입력된 키와 같습니다.");
+                        isSame = true;
+                    }
+                    else
+                    {
+                        Debug.Log("현재 입력된 키는 마지막으로 입력된 키와 다릅니다.");
+                        isSame = false;
+                        lastInputTime = Time.time;
+                        lastInput = keyCode;
+                    }
                 }
             }
         }
-        */
-        if (playerState.moveState == MoveState.idle )
-        {
-            
-            OnkeyPlayerMove();
-        }
-
+        return isSame;
     }
+    */
     public void OnkeyPlayerMove()
     {
         AxisInput();
         int nexty = (int)(playerObj.transform.position.y + moveDirection.y);
         int nextx = (int)(playerObj.transform.position.x + moveDirection.x);
-        Vector2 next = new Vector2(nextx,nexty);
+        Vector2 next = new Vector2(nextx, nexty);
+        playerState.moveState = MoveState.move;
         if (IsInSize())
         {
             switch (mapScript.map[nexty, nextx])
@@ -93,12 +112,12 @@ public class InputManager : MonoBehaviour
     }
     bool IsInSize()
     {
-        bool temp=false;
+        bool temp = false;
         int nexty = (int)(playerObj.transform.position.y + moveDirection.y);
         int nextx = (int)(playerObj.transform.position.x + moveDirection.x);
-        if ((nextx < mapScript.xSize && nexty < mapScript.ySize) && (nextx>-1 && nexty>-1))
+        if ((nextx < mapScript.xSize && nexty < mapScript.ySize) && (nextx > -1 && nexty > -1))
         {
-            temp = true;    
+            temp = true;
         }
         return temp;
     }
@@ -120,14 +139,13 @@ public class InputManager : MonoBehaviour
     public IEnumerator Moving()
     {
         playerPos = playerObj.transform.position;
-        playerState.moveState = MoveState.move;
         float maxDistance = SetMoveDistance(moveDirection);
         mapScript.TileInfoSwap(playerPos, playerPos + moveDirection, mapScript.playerPos, mapScript.tilePosList, TileType.player);
-        
+
         while (Vector2.Distance(playerPos + moveDirection, playerObj.transform.position) >= 0.2f)
         {
             playerObj.transform.Translate(moveDirection * Time.deltaTime * moveSpeed);
-            if(Vector2.Distance(playerPos + moveDirection, playerObj.transform.position) >= maxDistance)
+            if (Vector2.Distance(playerPos + moveDirection, playerObj.transform.position) >= maxDistance)
             {
                 Debug.Log("Break!");
                 break;
@@ -141,7 +159,7 @@ public class InputManager : MonoBehaviour
         }
         playerState.moveState = MoveState.idle;
     }
-    public void PlayerAttack(LivingEntity target,int damage)
+    public void PlayerAttack(LivingEntity target, int damage)
     {
         playerState.moveState = MoveState.attack;
         playerState.Attack(target);
@@ -150,14 +168,20 @@ public class InputManager : MonoBehaviour
         {
             EventManager.Instance.OnPlayerMove.Invoke();
         }
+        StartCoroutine("WaitInputTime");
+        Debug.Log(TurnManager.instance.turn);
         playerState.moveState = MoveState.idle;
 
+    }
+    IEnumerator WaitInputTime()
+    {
+        yield return new WaitForSeconds(1f);
     }
 
     float SetMoveDistance(Vector2 moveDirection)
     {
         float maxDixtance = 1.5f;
-        if (Mathf.Abs(moveDirection.x)==Mathf.Abs(moveDirection.y))
+        if (Mathf.Abs(moveDirection.x) == Mathf.Abs(moveDirection.y))
         {
             maxDixtance = 1.4f;
         }
@@ -174,7 +198,7 @@ public class InputManager : MonoBehaviour
         col = Physics2D.OverlapCircleAll(next, 0.1f); // 0.1f크기의 콜라이더 생성 해 다음위치의 오브젝트들 잡아옴
         foreach (Collider2D collider in col)
         {
-            if(collider.CompareTag("Monster"))
+            if (collider.CompareTag("Monster"))
             {
                 Debug.Log("Find Target");
                 collider.gameObject.TryGetComponent<LivingEntity>(out entity);
